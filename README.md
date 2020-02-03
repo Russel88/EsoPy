3f6d58a7784fbc7a7ed44c18d7417977d75133e6
Jakob Russel <russel2620@gmail.com>
2020-02-03 13:32:43 +0100

    update

--- README.md
+++ README.md
@@ -4,8 +4,27 @@ Esoteric python scripts
 ## mask_fasta
 Replace specific parts of a fasta file with N's to mask it in a BLAST (or BLAST-like) search
 
+```sh
+./mask_fasta.py input.fasta mask.csv output.fasta
+```
+The mask.csv file should be a comma-delimeted file with information on where to mask formatted as: fastaID,start,end. Start and end positions are inclusive in the masking
+
 ## cdhit_cluster_parse
 Parse the clustering output from CDHIT to a tab-delimted table
+```sh
+./cdhit_cluster_parse.py cdhit.clstr
+```
 
 ## crispr_repeat_cluster
 Cluster BLAST matches of repeat sequences to putative CRISPR arrays
+```sh
+./crispr_repeat_cluster.py -h
+```
+
+## minced_parser
+Parse output from minced to get all spacers in a fasta, all repeats in a fasta, or just a tab-delimeted files with positions of crisprs
+```sh
+./minced_parser.py minced.out tab
+./minced_parser.py minced.out repeats
+./minced_parser.py minced.out spacers
+```
---
+++ minced_parser.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python
+
+import sys
+import re
+
+# Define the CRISPR class
+class CRISPR:
+    count = 0
+    def __init__(self, sequence):
+        self.sequence = sequence.rstrip()
+        CRISPR.count += 1
+        self.crispr = 'CRISPR_{}'.format(CRISPR.count)
+        self.repeats = []
+        self.spacers = []
+    def setPos(self, start, end):
+        self.start = start.rstrip()
+        self.end = end.rstrip()
+    def addRepeat(self, repeat):
+        self.repeats.append(repeat.rstrip())
+    def addSpacer(self, spacer):
+        self.spacers.append(spacer.rstrip())
+
+
+# Read file line by line
+file = open(sys.argv[1], 'r')
+
+crisprs = []
+for ll in file:
+    # Record sequence accession
+    if ll.startswith('Sequence'):
+        sequence_current = re.sub('\' \(.*', '', re.sub('Sequence \'', '', ll))
+    # Create instance of CRISPR and add positions
+    if ll.startswith('CRISPR'):
+        crisp_tmp = CRISPR(sequence_current)
+        pos = re.sub('.*Range: ', '', ll)
+        start = re.sub(' - .*', '', pos)
+        end = re.sub('.* - ', '', pos)
+        crisp_tmp.setPos(start, end)
+    # Add Repeats and Spacers to the current instance
+    if ll[:1].isdigit():
+        lll = ll.split()
+        if len(lll) == 7:
+            crisp_tmp.addRepeat(lll[1])
+            crisp_tmp.addSpacer(lll[2])
+        if len(lll) == 2:
+            crisp_tmp.addRepeat(lll[1])
+    # Save the instance
+    if ll.startswith('Repeats'):
+        crisprs.append(crisp_tmp)
+
+# Output
+def getSeq(what):
+    for crisp in crisprs:
+        n = 0
+        for sq in getattr(crisp, what):
+            n += 1
+            print('>' + crisp.crispr + '@' + crisp.sequence + '_' + str(n) + '\n' + sq)
+
+
+if sys.argv[2] == 'tab':
+    for crisp in crisprs:
+        print(crisp.crispr + '\t' + crisp.sequence + '\t' + crisp.start + '\t' + crisp.end)
+if sys.argv[2] == 'repeats':
+    getSeq('repeats')
+if sys.argv[2] == 'spacers':
+    getSeq('spacers')
+
